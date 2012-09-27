134b976d6e980008eb85c05ae262d4b23bc14c92
Venkata Koppaka <subbarao.cse@gmail.com>
2012-09-26 18:59:17 -0700

    Adding XUnit

---
+++ BMI.Tests/BmiCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Bmi.Tests
+{
+    public class BmiCalculatorTests
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            int i = 10;
+            int j = 20;
+
+            Assert.Equal(30, i + j);
+        }
+    }
+}
